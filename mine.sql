246dc899a105c094ce53a4abe98e7ce7b93e55dd
joao <00001124020792sp@al.educacao.sp.gov.br>
2026-04-14 20:09:03 -0300

    inicio banco de dados

---
+++ mine.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Estudantes(
+	id SERIAL PRIMARY KEY,
+	nome TEXT NOT NULL ,
+	curso TEXT NOT NULL ,
+	idade INT 
+	
+);
+
+
+INSERT INTO Estudantes (id, nome, curso, idade) VALUES
+(1, 'joao', 'minecraft', 16),
+(2,'ana', 'minecraft', 15),
+(3, 'stephanie', 'programa', 2),
+(4, 'manzilli', 'programa', 69),
+(5, 'branco pedro alves', 'tecnico bilingue', 30)
+
+select * from Estudantes
